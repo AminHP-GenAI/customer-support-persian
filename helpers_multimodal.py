ad890fbc75e32a31d46859818ab9972f14b2378a
AminHP <mdan.hagh@gmail.com>
2024-05-24 00:03:35 +0330

    Add speech helpers

--- helpers_multimodal.py
+++ helpers_multimodal.py
@@ -4,6 +4,10 @@ from langchain_core.language_models import BaseChatModel
 from transformers import AutoProcessor, AutoModelForVision2Seq
 from transformers.image_utils import load_image
 
+from hezar.models import Model as AsrModel
+
+from TTS.api import TTS as _TTS
+
 from llm_translation import translate_to_english
 
 
@@ -72,3 +76,43 @@ class ImageInformationExtractor:
         new_height = int(image.height * scale)
         resize_image = image.resize((new_width, new_height))
         return resize_image
+
+
+############################################################################
+############################################################################
+
+
+class PersianAutomaticSpeechRecognizer:
+
+    def __init__(self) -> None:
+        self.model = AsrModel.load('hezarai/whisper-small-fa')
+
+    def invoke(self, audio_file_path: str) -> str:
+        output = self.model.predict(audio_file_path)
+        transcript = output[0]['text']
+        return transcript
+
+
+############################################################################
+############################################################################
+
+
+class TTS(_TTS):
+
+    @property
+    def is_multi_lingual(self):
+        return False
+
+
+class PersianTextToSpeech:
+
+    def __init__(self) -> None:
+        self.model = TTS(
+            model_path="storage/models/tts/checkpoint_88000.pth",
+            config_path="storage/models/tts/config.json"
+        )
+
+    def invoke(self, text: str, output_file_path: str) -> None:
+        if not output_file_path.endswith('.wav'):
+            output_file_path += '.wav'
+        self.model.tts_to_file(text, file_path=output_file_path)
